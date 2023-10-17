272d909adc773a3d802536f2ef5fbd8d920c2822
Srushti Jalgaonkar <147828302+Srushti1603@users.noreply.github.com>
2023-10-17 15:00:01 +0530

    Add files via upload
    Operators in java.

---
+++ Basics/operator.java
@@ -0,0 +1,12 @@
+public class operator {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 2;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+    
+}
