fc5a32129cbabde7b7b34ff8160d23cc8e69a916
Srushti Jalgaonkar <147828302+Srushti1603@users.noreply.github.com>
2023-10-17 15:01:19 +0530

    Add files via upload
    This program simply shows that how to take input from user.

---
+++ Basics/input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        System.out.println("Enter your age : ");
+        Scanner input = new Scanner(System.in);
+        int age = input.nextInt();
+        System.out.println("My age is : " +age);
+    }
+    
+}
