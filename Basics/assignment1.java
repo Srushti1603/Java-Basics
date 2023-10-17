dafd19fa5404c645a9ce66fbd7b0db0948d510ba
Srushti Jalgaonkar <147828302+Srushti1603@users.noreply.github.com>
2023-10-17 14:58:02 +0530

    Add files via upload
    A program to print name and age.

---
+++ Basics/assignment1.java
@@ -0,0 +1,7 @@
+public class assignment1{
+    public static void main(String[] args) 
+    {
+        System.out.println("My name is \"Srushti\"");
+        System.out.println("My age is 20");
+    }
+}
