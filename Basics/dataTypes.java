e2af7f3db76050143947463c572a19740d9cc8ed
Srushti Jalgaonkar <147828302+Srushti1603@users.noreply.github.com>
2023-10-17 14:59:14 +0530

    Add files via upload
    This program gives a basic introduction to Data Types in java.

---
+++ Basics/dataTypes.java
@@ -0,0 +1,17 @@
+public class dataTypes 
+{
+    public static void main(String[] args) {
+
+        int age = 20;
+        System.out.println(age);
+
+        double p = 3.14;
+        System.out.println(p);
+
+        char Fl = 'A';
+        System.out.println(Fl);
+
+        boolean breakfast = false;
+        System.out.println(breakfast);
+    }
+}
