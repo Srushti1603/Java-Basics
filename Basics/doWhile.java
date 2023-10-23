ec40d7adb4dbea8f68d0b4cec226ff488a0a8c70
Srushti Jalgaonkar <147828302+Srushti1603@users.noreply.github.com>
2023-10-23 13:48:43 +0530

    Add files via upload

---
+++ Basics/doWhile.java
@@ -0,0 +1,9 @@
+public class doWhile {
+    public static void main(String[] args) {
+        int i = 5;
+        do{
+            System.out.println("Count for do while loop ->"+i);
+            i--;
+        }while(i>=1);
+    }
+}
