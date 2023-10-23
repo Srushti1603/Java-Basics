29dfce87ba0a88733c80f5708b54b5d277d6fa36
Srushti Jalgaonkar <147828302+Srushti1603@users.noreply.github.com>
2023-10-23 13:48:13 +0530

    Add files via upload

---
+++ Basics/whileLoop.java
@@ -0,0 +1,11 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        int count = 5;
+        while(count>=1)
+        {
+            System.out.println("Count ->"+count);
+            count--;
+        }
+    }
+}
+ 
