8db03aff6a9cf0ba23e51d2bff13f11dc728f20e
Srushti Jalgaonkar <147828302+Srushti1603@users.noreply.github.com>
2023-10-23 13:47:54 +0530

    Add files via upload

---
+++ Basics/forLoop.java
@@ -0,0 +1,11 @@
+public class forLoop {
+    public static void main(String[] args) {
+        System.out.println("Odd numbers from 1 to 100");
+        int i;
+        for(i=1;i<=100;i=i+2)
+        {
+            System.out.println(i);
+        }
+    }
+}
+ 
