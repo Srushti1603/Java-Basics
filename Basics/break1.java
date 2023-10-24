c86f3716df8c8781b289f9f219d742f6844b0422
Srushti Jalgaonkar <147828302+Srushti1603@users.noreply.github.com>
2023-10-24 10:57:08 +0530

    Add files via upload

---
+++ Basics/break1.java
@@ -0,0 +1,14 @@
+public class break1 {
+    public static void main(String[] args) {
+        int s;
+        for(s=1;s<=30;s++)
+        {
+            if(s==16)
+            {
+                System.out.println("Party!!!");
+                break;
+            }
+            System.out.println(s);
+        }
+    }
+}
