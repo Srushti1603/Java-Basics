760ad35b69818846943e7bf6a1bda46cbb46a368
Srushti Jalgaonkar <147828302+Srushti1603@users.noreply.github.com>
2023-10-24 10:57:27 +0530

    Add files via upload

---
+++ Basics/continue1.java
@@ -0,0 +1,16 @@
+public class continue1 {
+        public static void main(String[] args) {
+            int s;
+            for(s=1;s<=30;s++)
+            {
+                if(s==16)
+                {
+                    System.out.println("Party!!!");
+                    continue;
+                }
+                System.out.println(s);
+            }
+    }
+    
+    
+}
