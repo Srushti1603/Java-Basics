59ceb9339feca72a741505c85ee89975cf5ff68f
Srushti Jalgaonkar <147828302+Srushti1603@users.noreply.github.com>
2023-10-25 20:43:21 +0530

    Add files via upload

---
+++ Basics/fun.java
@@ -0,0 +1,10 @@
+ //code inside class--
+public class fun{
+    static void greet()
+    {
+        System.out.println("Hello!");
+    }
+    public static void main(String[] args) {
+        greet();
+    }
+}
